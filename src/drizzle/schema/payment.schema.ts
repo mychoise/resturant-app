b4e327759830d280c585682ad00a2f4e5714991f
mychoise <dklsabin23@gmail.com>
2026-06-27 14:32:14 +0545

    updated payment schema

--- src/drizzle/schema/payment.schema.ts
+++ src/drizzle/schema/payment.schema.ts
@@ -1,6 +1,9 @@
 import { integer, timestamp, uuid } from 'drizzle-orm/pg-core';
 import { pgTable } from 'drizzle-orm/pg-core';
 import { order } from './order.schema';
+import { pgEnum } from 'drizzle-orm/pg-core';
+
+export const payment_type = pgEnum('payment_type', ['cash', 'online']);
 
 export const payment = pgTable('payment', {
   id: uuid('id').primaryKey().defaultRandom(),
@@ -8,6 +11,7 @@ export const payment = pgTable('payment', {
     .references(() => order.id)
     .notNull(),
   total_price: integer('total_price').notNull(),
-  transaction_id: uuid('transaction_id').notNull(),
+  payment_type: payment_type('payment_type').notNull(),
+  transaction_id: uuid('transaction_id'),
   created_at: timestamp('created_at').defaultNow(),
 });
