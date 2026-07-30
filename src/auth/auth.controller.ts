6056a7da282f7293037d814a5f4b1292cd7666cd
mychoise <dklsabin23@gmail.com>
2026-07-30 19:10:49 +0545

    updated for production

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -38,7 +38,7 @@ export class AuthController {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -54,7 +54,7 @@ export class AuthController {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
